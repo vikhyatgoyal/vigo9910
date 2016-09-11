aa1f5edee08d476d5ec9aa1cb3b98a0fe67c5694
vikhyat goyal <vigo9910@colorado.edu>
2016-09-11 17:18:19 -0600

    structured the code.

--- hw1/hw1.h
+++ hw1/hw1.h
@@ -2,10 +2,7 @@
 #define _HW1_H
 
 
-<<<<<<< 87382e008b6176f6540f28aab7bfe39addd0641d
-char reverse_string(char* str, int length);
-=======
 char reverse_string(char *str, int length);
->>>>>>> modified string reverse header and sourse file.
+int stringlen(char *str);
 
 #endif
