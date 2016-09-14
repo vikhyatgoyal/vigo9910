4d3e611fb8c4a35a9312413e99c2c6431a0d61e3
vikhyat goyal <vigo9910@colorado.edu>
2016-09-14 01:28:02 -0600

    .

--- hw1/main.c
+++ hw1/main.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
-<<<<<<< 4b217490ca2e4abc07ba7e9fd6ef4300402eb486
-=======
 #include "hw1.h"
 #define MAX_LEN (100)
->>>>>>> Reverse string finction added to teh main .c file
 
 
 
 int main()
 {
-<<<<<<< 4b217490ca2e4abc07ba7e9fd6ef4300402eb486
+
 char *ptrchar;
 int *ptrint;
 float *ptrfloat;
 int8_t *ptr8si;
 int16_t *ptr16si;
 int32_t *ptr32si;
-=======
- char arr[MAX_LEN];
+char arr[MAX_LEN];
  int arrlen,i=0,j=0;
  char finalreslt;
 
 
->>>>>>> Reverse string finction added to teh main .c file
+
 printf("size of char  - %lu \n",sizeof(char));
 printf("size of int   - %lu \n",sizeof(int));
 printf("size of float - %lu \n",sizeof(float));
@@ -36,7 +32,6 @@ printf("size of int8_t   - %lu \n",sizeof(int8_t));
 printf("size of uint8_t  - %lu \n",sizeof(uint8_t));
 printf("size of uint16_t - %lu \n",sizeof(uint16_t));
 printf("size of uint32_t - %lu \n",sizeof(uint32_t));
-<<<<<<< 4b217490ca2e4abc07ba7e9fd6ef4300402eb486
 printf("size of char*    - %lu \n",sizeof(ptrchar));
 printf("size of int*     - %lu \n",sizeof(ptrint));
 printf("size of float*   - %lu \n",sizeof(ptrfloat));
@@ -44,7 +39,6 @@ printf("size of void*    - %lu \n",sizeof(void*));
 printf("size of int8_t*  - %lu \n",sizeof(ptr8si));
 printf("size of int16_t* - %lu \n",sizeof(ptr16si));
 printf("size of int32_t* - %lu \n",sizeof(ptr32si));
-=======
 printf("size of char*    - %lu \n",sizeof(char *));
 printf("size of int*     - %lu \n",sizeof(int *));
 printf("size of float*   - %lu \n",sizeof(float *));
@@ -70,7 +64,6 @@ printf("size of int32_t* - %lu \n",sizeof(int32_t *));
    printf("invalid string");
    }
 
->>>>>>> Reverse string finction added to teh main .c file
 return 0;
 }
 
